18dd86c2767252ff82cbfcd01693d85cfafb1f04
Rafael Ferrés <rafael.ferres-jum@parceiros-kroton.com.br>
2021-03-16 14:53:13 -0300

    First project rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn soma(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn main() {
+    let name: &str = "World";
+    let _idade: i64 = 18i64;
+    let _cores: Vec<&str> = vec!["red", "green"];
+    println!("{}", soma(10, 10));
+    println!("Hello {}!", name);
+}
